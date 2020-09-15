d9a1867b288d8ffd7479d820b6a6da8a3a9fd175
AndreasAtakan <andreas@napkingis.no>
2020-09-16 01:01:37 +0200

    Added demo-data

---
+++ demos/transform.py
@@ -0,0 +1,41 @@
+# /*©agpl*************************************************************************
+# *                                                                              *
+# * Napkin Visual – Visualisation platform for the Napkin platform               *
+# * Copyright (C) 2020  Napkin AS                                                *
+# *                                                                              *
+# * This program is free software: you can redistribute it and/or modify         *
+# * it under the terms of the GNU Affero General Public License as published by  *
+# * the Free Software Foundation, either version 3 of the License, or            *
+# * (at your option) any later version.                                          *
+# *                                                                              *
+# * This program is distributed in the hope that it will be useful,              *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 *
+# * GNU Affero General Public License for more details.                          *
+# *                                                                              *
+# * You should have received a copy of the GNU Affero General Public License     *
+# * along with this program. If not, see <http://www.gnu.org/licenses/>.         *
+# *                                                                              *
+# *****************************************************************************©*/
+
+import csv
+import json
+
+
+def main():
+	folder = '[REPLACE]'
+
+	with open(f'{folder}/raw.csv', 'r') as inpfile, open(f'{folder}/DATA.geojson', 'w') as outfile:
+		reader = csv.reader(inpfile, delimiter=',')
+		data = { "type": "FeatureCollection", "features": [] }
+
+		for row in reader:
+			data['features'].append(
+				json.loads(row[0])
+			)
+
+		json.dump(data, outfile)
+
+
+if __name__ == '__main__':
+	main()
