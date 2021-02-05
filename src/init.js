8feb3ec93cd6c9b00d21ea7c3068484be1dbb51b
AndreasAtakan <andreas@napkingis.no>
2021-02-05 17:06:37 +0100

    indent fix

--- src/init.js
+++ src/init.js
@@ -24,93 +24,93 @@ const MAPBOX_TOKEN = 'pk.eyJ1IjoiYW5kcmVhc2F0YWthbiIsImEiOiJjazlndzM1cmUwMnl5M21
 
 /** STORE **/
 const reducers = (function createReducers(redux, keplerGl) {
-  return redux.combineReducers({
-    keplerGl: keplerGl.keplerGlReducer.initialState({
-      mapState: {
-        latitude: 60,
-        longitude: 8,
-        zoom: 5
-      },
-      mapStyle: {
-        styleType: 'satellite',
-        threeDBuildingColor: [ 192, 192, 192 ]
-      },
-      uiState: {
-        currentModal: null,
-        //activeSidePanel: false,
-        readOnly: false
-      }
-    })
-  });
+	return redux.combineReducers({
+		keplerGl: keplerGl.keplerGlReducer.initialState({
+			mapState: {
+				latitude: 60,
+				longitude: 8,
+				zoom: 5
+			},
+			mapStyle: {
+				styleType: 'satellite',
+				threeDBuildingColor: [ 192, 192, 192 ]
+			},
+			uiState: {
+				currentModal: null,
+				//activeSidePanel: false,
+				readOnly: false
+			}
+		})
+	});
 }(Redux, KeplerGl));
 
 const middleWares = (function createMiddlewares(keplerGl) {
-  return keplerGl.enhanceReduxMiddleware([
-    // Add other middlewares here
-  ]);
+	return keplerGl.enhanceReduxMiddleware([
+		// Add other middlewares here
+	]);
 }(KeplerGl));
 
 const enhancers = (function craeteEnhancers(redux, middles) {
-  return redux.applyMiddleware(...middles);
+	return redux.applyMiddleware(...middles);
 }(Redux, middleWares));
 
 const store = (function createStore(redux, enhancers) {
-  const initialState = {};
+	const initialState = {};
 
-  return redux.createStore(
-    reducers,
-    initialState,
-    redux.compose(enhancers)
-  );
+	return redux.createStore(
+		reducers,
+		initialState,
+		redux.compose(enhancers)
+	);
 }(Redux, enhancers));
 /** END STORE **/
 
 /** COMPONENTS **/
 const KeplerElement = (function(react, keplerGl, mapboxToken) {
-  return function(props) {
-    let rootElm = react.useRef(null);
+	return function(props) {
+		let rootElm = react.useRef(null);
 
-    let _useState = react.useState({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
+		let _useState = react.useState({
+			width: window.innerWidth,
+			height: window.innerHeight
+		});
 
-    let windowDimension = _useState[0],
-        setDimension = _useState[1];
+		let windowDimension = _useState[0],
+				setDimension = _useState[1];
 
-    react.useEffect(function sideEffect() {
-      function handleResize() {
-        setDimension({ width: window.innerWidth, height: window.innerHeight });
-      };
-      window.addEventListener('resize', handleResize);
-      return function() { window.removeEventListener('resize', handleResize); };
-    }, []);
+		react.useEffect(function sideEffect() {
+			function handleResize() {
+				setDimension({ width: window.innerWidth, height: window.innerHeight });
+			};
+			window.addEventListener('resize', handleResize);
+			return function() { window.removeEventListener('resize', handleResize); };
+		}, []);
 
-    return react.createElement(
-      'div',
-      { style: { position: 'absolute', left: 0, width: '100vw', height: '100vh' } },
-      react.createElement(keplerGl.KeplerGl, {
-        mapboxApiAccessToken: mapboxToken,
-        id: 'map',
-        width: windowDimension.width,
-        height: windowDimension.height,
-        appName: 'Napkin Visual',
-        appWebsite: 'https://napkingis.no'
-      })
-    );
-  };
+		return react.createElement(
+			'div',
+			{ style: { position: 'absolute', left: 0, width: '100vw', height: '100vh' } },
+			react.createElement(keplerGl.KeplerGl, {
+				mapboxApiAccessToken: mapboxToken,
+				id: 'map',
+				width: windowDimension.width,
+				height: windowDimension.height,
+				appName: 'Napkin Visual',
+				appWebsite: 'https://napkingis.no'
+			})
+		);
+	};
 }(React, KeplerGl, MAPBOX_TOKEN));
 
 const app = (function createReactReduxProvider(react, reactRedux, KeplerElement) {
-  return react.createElement(
-    reactRedux.Provider,
-    { store },
-    react.createElement(KeplerElement, null)
-  )
+	return react.createElement(
+		reactRedux.Provider,
+		{ store },
+		react.createElement(KeplerElement, null)
+	)
 }(React, ReactRedux, KeplerElement));
 /** END COMPONENTS **/
 
 /** Render **/
 (function render(react, reactDOM, app) {
-  reactDOM.render(app, document.getElementById('app'));
+	reactDOM.render(app, document.getElementById('app'));
 }(React, ReactDOM, app));
--- src/main.js
+++ src/main.js
@@ -25,6 +25,6 @@
  * Interact with map store to customize data and behavior
  */
 (function customize(keplerGl, store) {
-  //store.dispatch(keplerGl.toggleSplitMap());
-  //let exportMapData = function() { return KeplerGl.KeplerGlSchema.save(store.getState().keplerGl.map); };
+	//store.dispatch(keplerGl.toggleSplitMap());
+	//let exportMapData = function() { return KeplerGl.KeplerGlSchema.save(store.getState().keplerGl.map); };
 }(KeplerGl, store))
